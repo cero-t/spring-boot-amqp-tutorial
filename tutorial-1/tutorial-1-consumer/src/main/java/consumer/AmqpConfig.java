9502a027a810fef33b6f7f50df8978d173c0aa40
aqt-s.tanimoto <tanimoto@acroquest.co.jp>
2015-08-01 19:39:52 +0900

    changed to annotation based

--- tutorial-1/tutorial-1-consumer/src/main/java/consumer/AmqpConfig.java
+++ tutorial-1/tutorial-1-consumer/src/main/java/consumer/AmqpConfig.java
@@ -1,19 +1,19 @@
 package consumer;
 
-import consumer.receiver.SimpleReceiver;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
+@EnableRabbit
 @Configuration
-public class AmqpConfig {
-    static final String QUEUE_NAME = "tutorial-1";
-
+public class AmqpConfig implements RabbitListenerConfigurer {
     @Bean
     RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
         return new RabbitAdmin(connectionFactory);
@@ -21,24 +21,20 @@ public class AmqpConfig {
 
     @Bean
     Queue queue(RabbitAdmin rabbitAdmin) {
-        Queue queue = new Queue(QUEUE_NAME, false);
+        Queue queue = new Queue("tutorial-1", false);
         rabbitAdmin.declareQueue(queue);
         return queue;
     }
 
-    @Bean
-    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, Queue queue, MessageListenerAdapter listenerAdapter) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueues(queue);
-        container.setMessageListener(listenerAdapter);
-        return container;
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
+        registrar.setMessageHandlerMethodFactory(myHandlerMethodFactory());
     }
 
     @Bean
-    MessageListenerAdapter listenerAdapter(SimpleReceiver receiver) {
-        MessageListenerAdapter messageListenerAdapter = new MessageListenerAdapter(receiver, new Jackson2JsonMessageConverter());
-        messageListenerAdapter.setDefaultListenerMethod("receive");
-        return messageListenerAdapter;
+    public DefaultMessageHandlerMethodFactory myHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setMessageConverter(new MappingJackson2MessageConverter());
+        return factory;
     }
 }
--- tutorial-1/tutorial-1-consumer/src/main/java/consumer/receiver/SimpleReceiver.java
+++ tutorial-1/tutorial-1-consumer/src/main/java/consumer/receiver/SimpleReceiver.java
@@ -1,10 +1,12 @@
 package consumer.receiver;
 
 import domain.Student;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleReceiver {
+    @RabbitListener(queues = "tutorial-1")
     public void receive(Student student) {
         System.out.println(student.id);
         System.out.println(student.name);
